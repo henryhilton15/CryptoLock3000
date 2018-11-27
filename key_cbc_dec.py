2c8529d58bcd42ad4d582197e640efe5013c2429
Sungmin Lee <smini0811@gmail.com>
2018-11-27 18:42:45 +0100

    Added cbc encrypt, decrypt, initloginobjects, and udpateloginobjects to master_password

--- key_cbc_dec.py
+++ key_cbc_dec.py
@@ -3,44 +3,49 @@ from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util import Padding
 
-master = ''
-encrypted = ''
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:],'hm:e:')
-except getopt.GetoptError:
-    print('Usage: key_cbc_dec.py -m <master> -e <encrypted>')
-    sys.exit(2)
+def cbc_encrypt(keystring, password):    
 
-for opt, arg in opts:
-    if opt == '-h':
-        print('Usage: key_cbc_dec.py -m <master> -e <encrypted>')
-        sys.exit()
-    elif opt == '-m':
-        master = arg
-    elif opt == '-e':
-        encrypted = arg
+    if len(keystring) != 16:
+        print('Error: Keystring must be 16 bytes')
 
+    if len(password) == 0:
+        print('Error: Password is missing.')
+        sys.exit(2)
+
+    # generate a random IV and encrypt it in ECB mode
+    iv = Random.get_random_bytes(AES.block_size)
+    key = keystring.encode('utf-8')
+    cipher_ECB = AES.new(key, AES.MODE_ECB)
+    enc_iv = cipher_ECB.encrypt(iv)
+
+    # create an AES-CBC cipher object
+    cipher_CBC = AES.new(key, AES.MODE_CBC, iv)
+
+    # add padding
+    padded_password = Padding.pad(password.encode('utf-8'), AES.block_size)
+
+    # encrypt the plaintext
+    encrypted_password = cipher_CBC.encrypt(padded_password)
     
-if len(master) == 0:
-    print('Error: Enter master password')
-    sys.exit(2)
+    encrypted = enc_iv + encrypted_password
 
-if len(encrypted) == 0:
-    print('Error: No password to decrypt')
-    sys.exit(2)
-	
-# decryption
-else:
-    print('Decrypting...', end='')
+    return encrypted
 
-    # read the padded iv and the padded ciphertext from the input file
-    ifile = open(inputfile, 'rb')
-    ciphertext = ifile.read()
-    ifile.close()
 
-    enc_iv = ciphertext[:AES.block_size]
-    ciphertext = ciphertext[AES.block_size:]
+def cbc_decrypt(keystring, encrypted):
+    
+    if len(keystring) == 0:
+        print('Error: Enter keystring')
+        sys.exit(2)
+
+    if len(encrypted) == 0:
+        print('Error: No password to decrypt')
+        sys.exit(2)
+
+
+    enc_iv = encrypted[:AES.block_size]
+    encrypted_password = encrypted[AES.block_size:]
 	
     # decrypt iv using AES_ECB
     key = keystring.encode('utf-8')
@@ -51,14 +56,8 @@ else:
     cipher_CBC = AES.new(key, AES.MODE_CBC, iv)
 
     # decrypt ciphertext
-    padded_plaintext = cipher_CBC.decrypt(ciphertext)
-    plaintext = Padding.unpad(padded_plaintext, AES.block_size)
-    plaintext = plaintext.decode('utf-8')
+    padded_password = cipher_CBC.decrypt(encrypted_password)
+    password = Padding.unpad(padded_password, AES.block_size)
+    password = password.decode('utf-8')
 	
-    # write out the plaintext obtained into the output file
-    ofile = open(outputfile, 'w')
-    ofile.write(plaintext)
-    ofile.close()
-
-print('Done.')
-
+    return password
--- master_password.py
+++ master_password.py
@@ -6,7 +6,8 @@ from Crypto.Protocol.KDF import PBKDF2
 
 masterpassword = ""
 operation = "create"
-logininfofile = "infofile.txt"
+loginInfoFile = "infofile.txt"
+loginInfoObjects = []
 
 try:
     opts, args = getopt.getopt(sys.argv[1:],'hcag')
@@ -112,6 +113,100 @@ def random_pw_gen():
 	pwlen = 16
 	randompw = "".join(random.sample(s,pwlen))
 	return randompw
+
+
+SEPARATOR = "|||"
+
+def format_loginInfo(loginInfo):
+	return loginInfo.username + SEPARATOR + loginInfo.url + SEPARATOR + loginInfo.password
+
+def parse_line(entry):
+	first_sep = entry.find(SEPARATOR, 0, len(entry) - 1)
+	second_sep = entry.find(SEPARATOR, first_sep + 3, len(entry) - 1)
+	username = entry[:first_sep]
+	url = entry[first_sep + 3 : second_sep]
+	password = entry[second_sep + 3 : len(entry) - 1]
+	newLogin = LoginInfo(username, url, password)
+	return newLogin
+
+class LoginInfo:
+	def __init__(self, username, url, password):
+		self.username = str(username)
+		self.url = str(url)
+		self.password = str(password)
+
+
+def init_login_objects():
+	for line in loginInfoFile:
+		loginInfo = parse_line(line)
+		loginInfoObjects.append(loginInfo)
+
+def update_login_file():
+	ofile = open(loginInfoFile, "w")
+	for loginInfo in loginInfoObjects:
+		line = format_loginInfo(loginInfo)
+		ofile.write(line)
+	ofile.close()
+
+
+def cbc_encrypt(keystring, password):    
+
+    if len(keystring) != 16:
+        print('Error: Keystring must be 16 bytes')
+
+    if len(password) == 0:
+        print('Error: Password is missing.')
+        sys.exit(2)
+
+    # generate a random IV and encrypt it in ECB mode
+    iv = Random.get_random_bytes(AES.block_size)
+    key = keystring.encode('utf-8')
+    cipher_ECB = AES.new(key, AES.MODE_ECB)
+    enc_iv = cipher_ECB.encrypt(iv)
+
+    # create an AES-CBC cipher object
+    cipher_CBC = AES.new(key, AES.MODE_CBC, iv)
+
+    # add padding
+    padded_password = Padding.pad(password.encode('utf-8'), AES.block_size)
+
+    # encrypt the plaintext
+    encrypted_password = cipher_CBC.encrypt(padded_password)
+    
+    encrypted = enc_iv + encrypted_password
+
+    return encrypted
+
+
+def cbc_decrypt(keystring, encrypted):
+    
+    if len(keystring) == 0:
+        print('Error: Enter keystring')
+        sys.exit(2)
+
+    if len(encrypted) == 0:
+        print('Error: No password to decrypt')
+        sys.exit(2)
+
+
+    enc_iv = encrypted[:AES.block_size]
+    encrypted_password = encrypted[AES.block_size:]
+	
+    # decrypt iv using AES_ECB
+    key = keystring.encode('utf-8')
+    cipher_ECB = AES.new(key, AES.MODE_ECB)
+    iv = cipher_ECB.decrypt(enc_iv)
+
+    # create AES-CBC cipher object
+    cipher_CBC = AES.new(key, AES.MODE_CBC, iv)
+
+    # decrypt ciphertext
+    padded_password = cipher_CBC.decrypt(encrypted_password)
+    password = Padding.unpad(padded_password, AES.block_size)
+    password = password.decode('utf-8')
+	
+    return password
+
 
 
 
